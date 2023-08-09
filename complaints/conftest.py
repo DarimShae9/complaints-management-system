72235347c37bf597e50f068f055cef1e87612d24
darimshae9 <krawczyk.kamil85@gmail.com>
2023-08-09 19:39:47 +0200

    more tests

--- complaints/conftest.py
+++ complaints/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from django.test import Client
 from django.contrib.auth import get_user_model
 User = get_user_model()
+from .models import Order, Company, Message
 
 @pytest.fixture
 def client():
@@ -9,10 +10,14 @@ def client():
     return client
 
 
+
+
 @pytest.fixture
 def new_user():
+    company = Company.objects.create(nip='123451')
     ctx = {
-        'is_active': 1
+        'is_active': 1,
+        'company_id': company.nip
     }
     user = User.objects.create_user(
         username='user123',
@@ -21,3 +26,57 @@ def new_user():
         **ctx
     )
     return user
+
+
+@pytest.fixture
+def admin_user():
+    ctx = {
+        'is_active': 1,
+        'is_staff': 1,
+        'is_superuser': 1
+    }
+    user = User.objects.create_user(
+        username='admin123',
+        email='admin123@sds.pl',
+        password='admin123',
+        **ctx
+    )
+    return user
+
+
+@pytest.fixture
+def all_users():
+    return User.objects.all()
+
+
+@pytest.fixture
+def order():
+    company = Company.objects.create(
+        name="asd",
+        nip="123"
+    )
+
+    more_data = {
+        'company_id': company.nip,
+    }
+    return Order.objects.create(
+        number='12345',
+        **more_data
+    )
+
+
+@pytest.fixture
+def messages():
+    return Message.objects.all()
+
+
+@pytest.fixture
+def all_order():
+    return Order.objects.all()
+
+
+@pytest.fixture
+def company():
+    return Company.objects.create(
+        nip='123'
+    )
--- complaints/tests.py
+++ complaints/tests.py
@@ -1,23 +1,73 @@
 import pytest
 from django.contrib.auth import get_user_model
 User = get_user_model()
-from django.test import Client
-from django.test import TestCase
+from pytest_django.asserts import assertQuerysetEqual
+from .function import update_modification_time, historic_entry
+from datetime import datetime
 # Create your tests here.
 
 
 def test_login_page(client):
+    """testing the login page"""
     response = client.get('/login/')
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
 def test_create_user(new_user):
+    """test user creation"""
     assert User.objects.get(username="user123") == new_user
 
 
 @pytest.mark.django_db
 def test_login(client, new_user):
+    """testing whether logging works"""
     response = client.post('/login/', {'login': "user123", 'password': '1234567890qwertyuiop'})
     assert response['Location'] == '/panel/home/'
+
+
+@pytest.mark.django_db
+def test_admin_user(client, new_user):
+    """test if an unusual user is not able to connect the page for the admin"""
+    client.force_login(new_user)
+    response = client.get('/adm/user/')
+    assert 'admin/no_permissions.html' in (t.name for t in response.templates)
+
+
+@pytest.mark.django_db
+def test_admin_user_for_admin(client, admin_user):
+    """test if the admin has the ability to connect the page for the admin"""
+    client.force_login(admin_user)
+    response = client.get('/adm/user/')
+    assert 'admin/user.html' in (t.name for t in response.templates)
+
+
+@pytest.mark.django_db
+def test_admin_user_loading_all_user(client, admin_user, all_users):
+    """test if admin can load all users"""
+    client.force_login(admin_user)
+    response = client.get('/adm/user/')
+    assertQuerysetEqual(response.context['users'], all_users)
+
+
+@pytest.mark.django_db
+def test_update_modification_time(order):
+    """test whether the time update function works"""
+    update_modification_time(order.id)
+    assert order.modification_date <= datetime.now()
+
+
+@pytest.mark.django_db
+def test_historic_entry(order, new_user, messages):
+    """test whether the history function works"""
+    historic_entry(order.id, new_user, 'text')
+    assert len(messages) == 1
+
+
+@pytest.mark.django_db
+def test_add_compaint_view(client, new_user, all_order):
+    """checking if adding a complaint works"""
+    client.force_login(new_user)
+    client.post('/panel/add-compaint/', {'number': '123123'})
+    assert len(all_order) == 1
 
